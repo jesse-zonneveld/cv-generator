1e4eefac27886b3257e4590f58a732220e939619
jesse-zonneveld <jessezonneveld@gmail.com>
2020-10-20 11:45:27 -0700

    Updates

---
+++ precache-manifest.d19a88ad33d565f368413b11db2789e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41cf5c89bd42244a94f1f1cc18bd1c4",
+    "url": "/cv-generator/index.html"
+  },
+  {
+    "revision": "df0ecd8c4ee6dc3cf016",
+    "url": "/cv-generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "04cfb8ad261a1cf79a3f",
+    "url": "/cv-generator/static/js/2.ad2547fc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/cv-generator/static/js/2.ad2547fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0ecd8c4ee6dc3cf016",
+    "url": "/cv-generator/static/js/main.efbb179f.chunk.js"
+  },
+  {
+    "revision": "7b9c8d8c6d8ad8f4953d",
+    "url": "/cv-generator/static/js/runtime-main.5fc965f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cv-generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
